166b631c1e78bd431834d7f86d932ab0323a658c
Dorian Dayvid Gomes Feitosa <dorian.dayvid@aluno.ufca.edu.br>
2026-02-09 17:55:52 +0000

    Adicionado Setor clínico

--- src/snh_project/core/patient.py
+++ src/snh_project/core/patient.py
@@ -4,7 +4,10 @@ from src.snh_project.database import Base
 from src.snh_project.core.base import AuditoriaMixin
 
 class SetorClinico:
-    pass
+    def __init__(self, nome, leito):
+        self.nome = nome
+        self.leito = leito
+    
 
 class Paciente(Base,AuditoriaMixin):
     __tablename__ = 'Paciente'
@@ -14,7 +17,7 @@ class Paciente(Base,AuditoriaMixin):
     dataNasc = Column(String, nullable=False)
     setorClinico = Column(String, nullable=False)
     leito = Column(Integer, nullable=False)
-    datain = Column(Integer, nullable=False)
+    datain = Column(DateTime, nullable=False)
     risco = Column(Boolean, nullable=False)
 
     criado_em = Column(DateTime, default=datetime.now)
@@ -27,3 +30,21 @@ class Paciente(Base,AuditoriaMixin):
         self.leito = leito
         self.datain = datain
         self.risco = risco
+    
+    @property
+    def leito(self) -> int:
+        return self._leito
+    
+    @property
+    def setorClinico(self) -> str:
+        return self._setorClinico
+
+    @property
+    def datain(self) -> datetime:
+        return self._datain
+    
+    @property
+    def risco(self) -> bool:
+        if self._setorClinico.lower() == "uti":
+            return True
+        return self._risco
